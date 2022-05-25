18fba2cd1056caa75b177659d721d4cc978c4014
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2022-05-25 11:30:30 -0500

    Commits for React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DivBecas from './components/DivBecas/DivBecas';
+import Cabecera from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Cabecera/>
+      <DivBecas/>
+    </div>
+  );
+}
+
+export default App;
