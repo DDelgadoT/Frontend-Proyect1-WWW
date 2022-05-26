2f52219ea915b368ffb096bab67a42a3875ab03a
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2022-05-26 14:08:32 -0500

    Adding the route to registration

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,6 +13,7 @@ function Cabecera(props){
                     <Nav.Link href="/">Becas populares</Nav.Link>
                     <Nav.Link href="/internacionales">Becas internacionales</Nav.Link>
                     <Nav.Link href="/nacionales">Becas nacionales</Nav.Link>
+                    <Nav.Link href="/registro">Registrar beca</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
