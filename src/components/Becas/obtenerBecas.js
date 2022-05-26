713778a4084c879027ee2ea52a416b3cf36e79c8
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2022-05-26 18:08:36 -0500

    Getting scholarships now with API URL

--- src/components/Becas/obtenerBecas.js
+++ src/components/Becas/obtenerBecas.js
@@ -1,17 +1,28 @@
+import { API_URL } from '../url' 
+
+let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTM2ODM5ODMsImlhdCI6MTY1MzU5NzU4MywiaXNzIjoiRUxBRE1JTiJ9.dyq7U6EdKNGiRLLRludrxM9rbUbxBwDw_JT3d5osJfs";
+
+let requestOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json',
+        "Authorization": 'Bearer ' + token
+    }
+};
+
 export async function obtenerBecas(){
-    return await fetch("http://127.0.0.1:8080/Beca/all")
+    return await fetch(API_URL + "/Beca/all", requestOptions)
     .then(response => response.json())
     .then(data => data)
 };
 
 export async function obtenerBecasNacionales(){
-    return await fetch("http://127.0.0.1:8080/Beca/categoria/nacionales")
+    return await fetch(API_URL + "/Beca/categoria/Nacional", requestOptions)
     .then(response => response.json())
     .then(data => data)
 };
 
 export async function obtenerBecasInternacionales(){
-    return await fetch("http://127.0.0.1:8080/Beca/categoria/internacionales")
+    return await fetch(API_URL + "/Beca/categoria/Internacional", requestOptions)
     .then(response => response.json())
     .then(data => data)
 };
