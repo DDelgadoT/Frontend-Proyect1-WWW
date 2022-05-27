5d0cc431e776884d080d0b00283a706c71ca28e7
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2022-05-27 16:04:14 -0500

    Changes to PUT scholarship

--- src/components/EditarBeca/putBeca.js
+++ src/components/EditarBeca/putBeca.js
@@ -2,21 +2,22 @@ import { API_URL } from '../url';
 
 let token = sessionStorage.getItem('token');
 
-export default function putBeca(cuerpo){
+export default function putBeca(cuerpo, id){
     const requestOptions = {
         method: 'PUT',
-        headers: { 'Content-Type': 'application/json',
+        headers: {'Content-Type': 'application/json',
             "Authorization": 'Bearer ' + token
         },
         body: JSON.stringify(cuerpo)
     };
 
     try {
-        fetch(API_URL + "/Beca", requestOptions)
+        fetch(API_URL + "/Beca/" + id, requestOptions)
         .then(response => response.json())
-        .then(data => console.log(data));
-        alert("Actualización exitoso");
-        window.location.replace("/populares")
+        .then(data => {
+            alert("Actualización exitoso");
+            window.location.replace("/populares");
+        })
     }catch(e){
         console.log(e);
     }
