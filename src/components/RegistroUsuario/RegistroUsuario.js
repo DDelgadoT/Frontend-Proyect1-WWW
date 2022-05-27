32e479b4f749d947030284b9d654e7f62657f54c
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2022-05-27 11:22:53 -0500

    Register user

--- src/components/RegistroUsuario/RegistroUsuario.js
+++ src/components/RegistroUsuario/RegistroUsuario.js
@@ -1,21 +1,45 @@
-import React from "react";
-import './RegistroUsuario.css';
-import { Container } from 'react-bootstrap';
-import RegistrarBeca from "../RegistrarBeca/RegistrarBeca";
-import Cabecera from '../Navbar/Navbar';
+import React from "react"
+import { Container, Form, Button } from 'react-bootstrap';
+import registrarUsuario from './registrarUsuario';
+import { CabeceraLogin } from "../Navbar/Navbar";
 
-function RegistroUsuario() {
-  return (
-    <>
-      <Cabecera />
-      <div className="App">
-        <h1 className="title">Registro de usuario</h1>
-      </div>
-      <Container fluid border="dark" className="border border-dark div">
-        <RegistrarBeca/>
-      </Container>
-    </>
-  );
+import './RegistroUsuario.css';
+
+function handleSubmit(event) {
+  event.preventDefault()
+
+  let body = {
+      email: event.target.elements.formCorreo.value,
+      password: event.target.elements.formContraseña.value,
+  }
+
+  registrarUsuario(body);
 }
 
-export default RegistroUsuario;
+function registroUsuario(){
+
+  return(
+    <>
+      <CabeceraLogin />
+      <div className="App">
+        <h1 className="title">Registrando usuario</h1>
+      </div>
+      <Container fluid border="dark" className="border border-dark div">
+        <h4>Ingrese los siguientes datos</h4>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formCorreo">
+            <Form.Label>Correo electrónico</Form.Label>
+              <Form.Control type="email" placeholder="Ingrese el correo electrónico" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formContraseña">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control type="password" placeholder="Ingrese la contraseña" />
+            </Form.Group>
+            <Button variant="primary" type="submit">Registrarse</Button>
+            
+        </Form>
+      </Container>
+    </>
+  )
+}
+export default registroUsuario;
