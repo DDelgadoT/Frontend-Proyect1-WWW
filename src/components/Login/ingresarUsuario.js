fa84c55175b69ce7361405d68fe2030fbe810475
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2022-05-27 15:10:23 -0500

    Login now shows if the data is incorrect

--- src/components/Login/ingresarUsuario.js
+++ src/components/Login/ingresarUsuario.js
@@ -12,8 +12,12 @@ export default function ingresarUsuario(cuerpo){
         fetch(API_URL + "/User/login", requestOptions)
         .then(response => response.json())
         .then(data => {
+            if(typeof data !== "string"){
             sessionStorage.setItem('token', data.token);
             window.location.replace("/populares");
+            }else{
+                alert("Datos incorrectos");
+            }
         })
     }catch(e){
         console.log(e);
