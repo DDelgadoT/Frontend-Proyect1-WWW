174231aa8a49a2238f0dba5b753bfc72660a9d9a
Sebastian <eljhoan1411@gmail.com>
2022-05-27 23:02:24 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import { Router } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
     <div className="App">
       <Login/>
     </div>
-    </Router>
   );
 }
 
